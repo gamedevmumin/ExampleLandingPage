0dfb9eaa2ffc4f691853600fd985ee123593766a
GameDevMumin <GameDevMumin@wp.pl>
2020-02-27 20:45:41 +0100

    Added hero image, changed some css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,19 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="hero">
-      <h1 className="hero__title"> Get our latest book here! </h1>
-      <p className="hero__description">
-        Learn how to code with our new book about webdevelopment.{" "}
-      </p>
-      <div className="hero__btn-container">
-        <button className="btn-primary">Download now</button>
+      <div className="hero__content-container">
+        <div>
+          <h1 className="hero__content-container__title">
+            Get our latest course here!
+          </h1>
+          <p className="hero__content-container__description">
+            Learn how to code with our new course about webdevelopment.
+          </p>
+          <div className="hero__content-container__btn-container">
+            <button className="btn-primary">Download now</button>
+          </div>
+        </div>
+        <div className="hero__content-container__image"></div>
       </div>
     </div>
   );
